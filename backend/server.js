e5372cff50576023453de950433679654771ad4a
Qasim-016 <pikachugaming565@gmail.com>
2025-09-22 18:01:08 +0500

    main

--- backend/server.js
+++ backend/server.js
@@ -147,7 +147,7 @@ app.get('/user/:userId', async (req, res) => {
 initializeSchedule();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
