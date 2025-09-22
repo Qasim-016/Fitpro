b67cb61f31f2cacedc39ab249feb82ac067a7f50
Qasim-016 <pikachugaming565@gmail.com>
2025-09-22 18:59:32 +0500

    main

---
+++ backend/models/u.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    uid: {
+    type: String,
+    required: true, // Ensure that firebaseId is always provided
+},
+  email: { type: String, required: true, unique: true ,lowercase:true},
+  username: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profileImage: { type: String, default: '' }, // Store Base64 string
+
+
+  
+}, { collection: 'fitpro' }); // Ensure it's saved in the 'fitpro' collectionss
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+  
+
+
+
