f0230a1f988d32f392e43eefd7d7c7b878a1e864
Qasim-016 <pikachugaming565@gmail.com>
2025-09-23 07:39:32 +0500

    main

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,19 @@ app.use(express.json({ limit: '10mb' }));  // Increase to 10MB
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 // Initialize Firebase Admin
-const serviceAccountPath = process.env.FIREBASE_ADMIN_KEY_PATH;
-
+const serviceAccountPath = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 if (!admin.apps.length) {
   try {
     const serviceAccount = require(path.resolve(serviceAccountPath));
